6a0597676b79ad562b1e4aac480927b9670b5ca8
msaadkiani <saadi.iqbal@gmail.com>
2021-07-15 22:09:05 +0500

    First Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'yuzee';
+export class AppComponent 
+{
+    cards = [
+        {heading: 'Taylors University World Class Scholarship'},
+        {heading: 'Summer Research Scholarship'},
+        {heading: 'Postgraduate Research Scholarship'},
+        {heading: 'Taylors University Mechanical Engineering Scholarship'},
+        {heading: 'The Gates Scholarship'}
+    ]
+
+    cardDetail = [
+        {detIcon: './assets/det-ic1.png', detHeading: '18 March 2020', detText: 'Deadline'},
+        {detIcon: './assets/det-ic2.png', detHeading: 'Domestic & International', detText: 'Validity'},
+        {detIcon: './assets/det-ic3.png', detHeading: 'USD 2915 per year', detText: 'Amount'},
+        {detIcon: './assets/det-ic4.png', detHeading: 'Degree  |  Postgraduate', detText: 'Study Level'},
+        {detIcon: './assets/det-ic5.png', detHeading: 'May 2020', detText: 'Intake(s)'}
+    ]
 }
